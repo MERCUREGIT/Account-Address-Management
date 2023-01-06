0607f470e4e317529a21503c68aa2703bbf898cb
mercuregit <ngumbukafon@gmail.com>
2023-01-06 15:35:18 +0100

    delete: UnUsed boundary component

--- force-app/main/default/lwc/boundary/boundary.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class Boundary extends LightningElement {
-    error;
-    stack;
-    errorCallback(error, stack) {
-        this.error = error;
-    }
-} 
